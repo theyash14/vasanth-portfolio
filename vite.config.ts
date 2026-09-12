960821f252b1ed8b94172e2137b4fbbaad1abde2
theyash14 <yashwanthr@proton.me>
2026-09-13 04:45:38 +0530

    chore: migrate to astro

--- vite.config.ts
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from "vite-plus";
-
-export default defineConfig({
-  staged: {
-    "*": "vp check --fix",
-  },
-  lint: { options: { typeAware: true, typeCheck: true } },
-});
