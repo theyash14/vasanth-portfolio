916717e40eba4fc4b5c1e75a96b21dbfd9b353fc
theyash14 <yashwanthr@proton.me>
2026-09-15 03:54:35 +0530

    feat: add full.dev components

---
+++ src/components/ui/header/index.ts
@@ -0,0 +1,3 @@
+export { default as Header } from "./header.astro"
+export { default as HeaderContainer } from "./header-container.astro"
+export { default as HeaderGroup } from "./header-group.astro"
