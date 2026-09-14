6e7f85dffb9effe2aab1b92b9449c55d0fea1f38
theyash14 <yashwanthr@proton.me>
2026-09-14 21:19:49 +0530

    feat: add cloudflare integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,8 @@ import { defineConfig } from 'astro/config';
 
 import tailwindcss from "@tailwindcss/vite";
 
+import cloudflare from "@astrojs/cloudflare";
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://www.example.com",
@@ -10,4 +12,6 @@ export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
   },
+
+  adapter: cloudflare(),
 });
